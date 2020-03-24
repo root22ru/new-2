79242403ca568edfde606791841aff6014362aeb
Sergei <serega-pro-1997@mail.ru>
2020-03-24 21:39:08 +0700

    Это наш первыый комит

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp' ];
+console.log(arr[0]);
+
+// let answer = confirm('Hi live');
+// console.log(answer);
+// let answer = +prompt('Ксли ли вам 18?', 'Да');
+// console.log(typeof(answer));
+
+// console.log("arr" + "- object");
+
+let incr = 10,
+    decr = 10;
+
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(5%2);
+    console.log('2' === '2');
+
+    let isChecked = true,
+        isClose = false;
+        console.log(isChecked && isClose);
